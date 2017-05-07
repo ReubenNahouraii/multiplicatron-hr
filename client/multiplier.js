364ba90d2dbfdc7047447f1b98fbe53e273dcf30
GrokN <FullGrok@gmail.com>
2017-05-07 04:25:20 -0700

    Add custom attribute to detect once a box loses focus

--- client/multiplier.js
+++ client/multiplier.js
@@ -14,7 +14,8 @@ class ViewManager {
         'click',
         () => {
           let div = document.createElement('div')
-          div.innerHTML = '<input type="text">'
+          div.innerHTML = '<input type="text" data-on="false" ' +
+            '               onblur="event.target.setAttribute(\'data-on\', \'true\')">'
 
           let lastNode = document.getElementById('renderValue')
           lastNode.parentNode.insertBefore(div, lastNode)
@@ -28,7 +29,9 @@ class ViewManager {
         divInputs = document.getElementById('form-upper').getElementsByTagName('div')
 
     Array.from(divInputs).forEach( div => {
-      value = multiply(value, parseInt(div.getElementsByTagName('input')[0].value))
+      let input = div.getElementsByTagName('input')[0]
+      if(input.getAttribute('data-on') === 'true')
+        value = multiply(value, parseInt(input.value))
     })
 
     this.renderValue(value)
